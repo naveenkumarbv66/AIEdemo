d26584369dcdc46b3b0e4ebc35cc107ad20d3ce5
Naveen Kumar BV <naveenkumarbv66@gmail.com>
2021-02-27 13:16:06 +0530

    android notificaiton logic implemented

---
+++ app/src/main/java/com/naveen/aiedemo/view/notifications/NotificationPublisher.kt
@@ -0,0 +1,62 @@
+package com.naveen.aiedemo.view.notifications
+
+import android.R
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.naveen.aiedemo.view.DashboardActivity
+
+@Suppress("DEPRECATION")
+class NotificationPublisher : BroadcastReceiver() {
+
+    companion object {
+        const val ACTION_NAME = "MyBroadcastReceiverAction"
+        const val NOTIFICATION_CHANNEL_ID = "App_AIE_notification"
+        const val NOTIFICATION_CHANNEL_NAME = "AIE"
+        const val NOTIFICATION_CHANNEL_DESCRIPTION = "AIE_Dec"
+        const val NOTIFICATION_ID = 66
+
+        const val TASK_NAME = "taskName"
+        const val TASK_DESCRIPTION = "taskDescription"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent?.action == ACTION_NAME) {
+            createNotificationChannel(context, intent)
+        }
+    }
+
+    private fun getNotification(context: Context?, intent: Intent?): Notification.Builder {
+        return Notification.Builder(context).apply {
+            setSmallIcon(R.drawable.ic_dialog_alert)
+            setContentTitle(intent?.getStringExtra(TASK_NAME))
+            setContentText(intent?.getStringExtra(TASK_DESCRIPTION))
+            setPriority(Notification.PRIORITY_DEFAULT)
+            setAutoCancel(true)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) setChannelId(NOTIFICATION_CHANNEL_ID)
+            setDefaults(Notification.DEFAULT_SOUND)
+            setContentIntent(PendingIntent.getActivity(context, NOTIFICATION_ID, Intent(context, DashboardActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT))
+        }
+    }
+
+    private fun createNotificationChannel(context: Context?, intent: Intent?) {
+        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationBuilder = getNotification(context, intent)
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, importance).apply {
+                description = NOTIFICATION_CHANNEL_DESCRIPTION
+            }
+            // Register the channel with the system
+            notificationManager.createNotificationChannel(channel)
+        }
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+    }
+}
--- app/src/main/java/com/naveen/aiedemo/view/todolist/TodoListFragment.kt
+++ app/src/main/java/com/naveen/aiedemo/view/todolist/TodoListFragment.kt
@@ -1,10 +1,17 @@
 package com.naveen.aiedemo.view.todolist
 
+import android.app.AlarmManager
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -17,12 +24,16 @@ import com.naveen.aiedemo.databinding.FragmentTodolistBinding
 import com.naveen.aiedemo.view.BaseFragment
 import com.naveen.aiedemo.view.adapter.TodoTaskListAdapter
 import com.naveen.aiedemo.view.listeners.RecyclerViewListener
+import com.naveen.aiedemo.view.notifications.NotificationPublisher
+import com.naveen.aiedemo.view.notifications.NotificationPublisher.Companion.ACTION_NAME
+import com.naveen.aiedemo.view.notifications.NotificationPublisher.Companion.NOTIFICATION_ID
+import com.naveen.aiedemo.view.notifications.NotificationPublisher.Companion.TASK_DESCRIPTION
+import com.naveen.aiedemo.view.notifications.NotificationPublisher.Companion.TASK_NAME
 import com.naveen.aiedemo.view.room.model.TodoTableModel
 import com.naveen.aiedemo.view.room.repository.TodoTaskRepositoryImpl
 import kotlinx.android.synthetic.main.fragment_todolist.*
-import com.naveen.aiedemo.view.datetimepicker.DateTimePickerDialog
-import java.text.SimpleDateFormat
 import java.util.*
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -30,18 +41,17 @@ import java.util.*
 class TodoListFragment : BaseFragment() {
 
     private val todoTaskViewModel: TodoTaskViewModel by activityViewModels()
-
     lateinit var binding: FragmentTodolistBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_todolist,
-            container,
-            false
+                inflater,
+                R.layout.fragment_todolist,
+                container,
+                false
         )
 
         binding.lifecycleOwner = this;
@@ -60,19 +70,20 @@ class TodoListFragment : BaseFragment() {
         todoListRecyclerView.adapter = TodoTaskListAdapter(object : RecyclerViewListener {
             override fun onClick(todoTaskObject: TodoTableModel) {
                 Log.d(
-                    "Naveen", "=====>>>\n Selected => ".plus(todoTaskObject.Id.toString())
+                        "Naveen", "=====>>>\n Selected => ".plus(todoTaskObject.Id.toString())
                         .plus(" : ").plus(todoTaskObject.TaskTitle)
                         .plus(" ~~~ ").plus(todoTaskObject.TaskInfo)
                 )
                 todoTaskViewModel.selectedTaskObject = todoTaskObject
-                findNavController().navigate(R.id.action_DisplayTaskListFragment_to_DisplaySingleTaskFragment)
+//                createNotificationAlaram(todoTaskObject)
+                 findNavController().navigate(R.id.action_DisplayTaskListFragment_to_DisplaySingleTaskFragment)
             }
         })
         todoListRecyclerView.addItemDecoration(
-            DividerItemDecoration(
-                context,
-                layoutManager.orientation
-            )
+                DividerItemDecoration(
+                        context,
+                        layoutManager.orientation
+                )
         )
 
         binding.viewModel = todoTaskViewModel
@@ -80,11 +91,11 @@ class TodoListFragment : BaseFragment() {
         todoTaskViewModel.liveDataTodoTableModelTest.observe(viewLifecycleOwner, {
             it.forEachIndexed { index: Int, todoTableModel: TodoTableModel ->
                 Log.d(
-                    "Naveen",
-                    "====> " + todoTableModel.Id.toString().plus(" => ").plus(
-                        todoTableModel.TaskTitle.plus(" : ")
-                            .plus(todoTableModel.TaskInfo)
-                    )
+                        "Naveen",
+                        "====> " + todoTableModel.Id.toString().plus(" => ").plus(
+                                todoTableModel.TaskTitle.plus(" : ")
+                                        .plus(todoTableModel.TaskInfo)
+                        )
                 )
             }
         })
@@ -96,13 +107,37 @@ class TodoListFragment : BaseFragment() {
     private fun getTodoCompleteList() {
         activity?.let { it1 ->
             todoTaskViewModel.getData(it1.applicationContext)
-                ?.observe(viewLifecycleOwner, {
-                    if (viewLifecycleOwner.lifecycle.currentState == Lifecycle.State.RESUMED) {
-                        if (it.isNullOrEmpty()) todoTaskViewModel.liveDataTodoTableModelTest.value =
-                            todoTaskViewModel.getIDefaultNoDataMessage()
-                        else todoTaskViewModel.liveDataTodoTableModelTest.value = it
-                    }
-                })
+                    ?.observe(viewLifecycleOwner, {
+                        if (viewLifecycleOwner.lifecycle.currentState == Lifecycle.State.RESUMED) {
+                            if (it.isNullOrEmpty()) todoTaskViewModel.liveDataTodoTableModelTest.value =
+                                    todoTaskViewModel.getIDefaultNoDataMessage()
+                            else todoTaskViewModel.liveDataTodoTableModelTest.value = it
+                        }
+                    })
         }
+    }
+
+    private fun createNotificationAlaram(todoTaskObject: TodoTableModel) {
+        val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, NotificationPublisher::class.java).apply {
+            putExtra(TASK_NAME, todoTaskObject.TaskTitle)
+            putExtra(TASK_DESCRIPTION, todoTaskObject.TaskInfo)
+        }
+        // Used for filtering inside Broadcast receiver
+        intent.action = ACTION_NAME
+        val pendingIntent = PendingIntent.getBroadcast(context, NOTIFICATION_ID, intent, PendingIntent.FLAG_CANCEL_CURRENT)
+        // In this particular example we are going to set it to trigger after 30 -> 30000 seconds.
+        // You can work with time later when you know this works for sure.
+        val msUntilTriggerHour: Long = 10000
+        val alarmTimeAtUTC: Long = System.currentTimeMillis() + msUntilTriggerHour
+        // Depending on the version of Android use different function for setting an Alarm.
+        // setAlarmClock() - used for everything lower than Android M
+        //alarmManager.setAlarmClock(AlarmManager.AlarmClockInfo(alarmTimeAtUTC, pendingIntent), pendingIntent)
+        // setExactAndAllowWhileIdle() - used for everything on Android M and higher
+        alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                alarmTimeAtUTC,
+                pendingIntent
+        )
     }
 }
